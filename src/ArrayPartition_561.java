b141d9aa1ff929891608f5fd07fb55b52cd1e504
Xiao Xiao <xiao81@purdue.edu>
2017-04-26 12:18:28 -0400

    First commit

---
+++ src/ArrayPartition_561.java
@@ -0,0 +1,6 @@
+/**
+ * Created by xiaoxiao on 4/25/17.
+ */
+public class ArrayPartition_561 {
+
+}
---
+++ src/hammingWeight.java
@@ -0,0 +1,18 @@
+/**
+ * Created by xiaoxiao on 3/1/17.
+ */
+public class hammingWeight {
+    public static void main(String[] args) {
+
+    }
+    public int hammingWeight(int n) {
+        int count = 0;
+        String bitString = Integer.toBinaryString(n);
+        for (int i = 0; i < bitString.length(); i++) {
+            if (bitString.charAt(i) == '1'){
+                count++;
+            }
+        }
+        return count;
+    }
+}
