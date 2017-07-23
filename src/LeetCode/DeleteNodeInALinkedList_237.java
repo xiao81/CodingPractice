a076974aa6e7eb059a08a00417d08aac246c85d8
Xiao Xiao <xiao81@purdue.edu>
2017-07-23 01:59:13 -0400

    July 23 1:58AM

---
+++ src/LeetCode/DeleteNodeInALinkedList_237.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class DeleteNodeInALinkedList_237 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        ListNode currNode = node;
+        while (currNode.next != null) {
+            if (currNode.next.next == null) {
+                currNode.val = currNode.next.val;
+                currNode.next = null;
+                return;
+            }
+            currNode.val = currNode.next.val;
+            currNode = currNode.next;
+        }
+
+    }
+    public void deleteNode1(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
