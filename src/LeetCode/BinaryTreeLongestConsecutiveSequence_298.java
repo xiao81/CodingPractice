51ab8a0c2181365243b3a7d2556aebfbc1301921
Xiao Xiao <xiao81@purdue.edu>
2017-07-28 02:09:56 -0400

    July 28 2:09AM

---
+++ src/LeetCode/BinaryTreeLongestConsecutiveSequence_298.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+public class BinaryTreeLongestConsecutiveSequence_298 {
+    private int result = 0;
+    public int longestConsecutive(TreeNode root) {
+        longestConsecutiveHelper(root);
+        return result;
+    }
+
+    private int longestConsecutiveHelper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int path = 1;
+        if (root.left != null) {
+            int left = longestConsecutiveHelper(root.left);
+            if (root.left.val - root.val == 1) path = left + 1;
+        }
+        if (root.right != null) {
+            int right = longestConsecutiveHelper(root.right);
+            if (root.right.val - root.val == 1) path = Math.max(path, right + 1);
+        }
+        result = Math.max(result, path);
+        return path;
+    }
+}
---
+++ src/LeetCode/BoundaryOfBinaryTree_545.java
@@ -0,0 +1,62 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class BoundaryOfBinaryTree_545 {
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        if (root == null) return result;
+        result.add(root.val);
+        if (root.left == null && root.right == null) {
+            return result;
+        }
+        findBoundary(root, true, 0);
+        findLeafNodes(root);
+        int index = result.size();
+        findBoundary(root, false, index);
+
+        return result;
+    }
+
+    private void findLeafNodes(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            result.add(root.val);
+        }
+        findLeafNodes(root.left);
+        findLeafNodes(root.right);
+    }
+
+    private void findBoundary(TreeNode root, boolean dir, int index) {
+        TreeNode curr = dir ? root.left : root.right;
+        while (curr != null) {
+            if (dir) {
+                if (curr.left != null) {
+                    result.add(curr.val);
+                    curr = curr.left;
+                } else if (curr.right != null){
+                    result.add(curr.val);
+                    curr = curr.right;
+                } else {
+                    break;
+                }
+            } else {
+                if (curr.right != null) {
+                    result.add(index, curr.val);
+                    curr = curr.right;
+                } else if (curr.left != null){
+                    result.add(index, curr.val);
+                    curr = curr.left;
+                } else {
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
